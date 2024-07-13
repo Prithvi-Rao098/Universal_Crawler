24ef4422c73fb0f570bad5ef70e1e3d79952d683
Prithvi_Rao <102133984+Prithvi-Rao098@users.noreply.github.com>
2024-07-12 19:41:46 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Universal_Crawler
+ 
---
+++ app.py
@@ -0,0 +1,32 @@
+from firecrawl import FirecrawlApp
+from openai import OpenAI
+from dotenv import load_dotenv
+import os
+import json
+import pandas as ad
+import datetime
+
+
+def scrape_data(url):
+    load_dotenv()
+
+    # Intialize FireCrawler
+    app = FirecrawlApp(api_key=os.getenv('FIRECRAWL_API_KEY'))
+    
+    # scrape the url
+    scrape_data = app.scrape_url(url)
+
+    # make sure the data has a 'markdown' key
+    if 'markdown' in scrape_data:
+        return scrape_data['markdown']
+    else:
+        raise KeyError("the url does not have scraped down data that can be marked down")
+
+# saving the data
+def save_data(raw_data, timestamp, output_folder = 'outputs'):
+    output_path = os.path.join(output_folder, f'raw_Data_{timestamp}.md')
+
+    with open(output_path, 'w', encoding='utf-8') as f:
+        f.write(raw_data)
+
+    print(f "the data from the website is saved to {output_path}")           
